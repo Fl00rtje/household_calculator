d91789872b5f96a817238e1e8adab0cb5c8a7ca4
Floor Harmsen <floor.harmsen@gmail.com>
2019-07-31 15:16:37 +0200

    changed names of files

---
+++ notes.py
@@ -0,0 +1,19 @@
+#vragen of gas en licht bij elkaar is of apart
+#vragen of tv, internet en bellen samen zijn of apart
+
+#mobiele telefoon
+
+#basisverzekering
+#aanvullende verzekering
+#reisverzekering
+#annuleringsverzekering
+#rechtsbijstandsverzekering
+
+#abonnement krant
+#abonnement tijdschrift
+#overige abonnementen
+
+
+
+
+
